08ed800fbaa4194022b43eeefb68194fe575fd07
acmilanfan <gentoo1993@gmail.com>
2019-01-27 13:05:49 +0300

    empty backbone with configured spring boot + postgres docker environment

---
+++ src/main/kotlin/com/gentooway/blog/web/HealthController.kt
@@ -0,0 +1,13 @@
+package com.gentooway.blog.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun test(): String {
+        return "OK"
+    }
+}
